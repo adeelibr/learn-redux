71acf40cac65215722a970a9780470c2fe8d8ed9
adeelibr <adeelimranr@gmail.com>
2017-02-14 03:05:54 +0500

    added store, use getCurrentStore() func to get the current srtore object

--- src/ReduxExample.js
+++ src/ReduxExample.js
@@ -1,3 +1,11 @@
-import redux from 'redux';
+import { createStore } from 'redux';
 
-console.log('Starting redux examples');
+console.log('Starting Redux Examples');
+
+let reducer = (state = { name: 'Anonymous' }, action) => {
+    return state;
+}
+let store = createStore(reducer);
+
+let currentState = store.getState();
+console.log('currentState ', currentState);
