e79af2d85a9b2e3d4661c6108dd92dee6a0534e3
adeelibr <adeelimranr@gmail.com>
2017-02-14 01:43:38 +0500

    Pure & Impure Function basic example

--- src/ReduxExample.js
+++ src/ReduxExample.js
@@ -3,6 +3,10 @@ import redux from 'redux';
 console.log('Starting redux example');
 
 // Pure Function
-function add(a, b) {
-    return a + b;
-}
+add = (a, b) => a + b;
+console.log('(Pure Function) Add 4+4= ', add(4,4));
+
+// Impure Function
+let a = 3;
+add = (b) => a + b;
+console.log('(Impure Function) Add 3+4= ', add(4));
