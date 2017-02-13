f139230facf8d9ef9bfa4701aa66e6533d432764
adeelibr <adeelimranr@gmail.com>
2017-02-14 03:27:38 +0500

    Add reducer and start working with store

--- src/ReduxExample.js
+++ src/ReduxExample.js
@@ -1,11 +1,27 @@
+// import { createStore } from 'redux';
+
+// console.log('Starting Redux Examples');
+
+// let reducer = ( state = { name: 'Anonymous' }, action) => {
+//     return state;
+// }
+// let store = createStore(reducer);
+
+// let currentState = store.getState();
+// console.log('currentState ', currentState);
+
 import { createStore } from 'redux';
 
-console.log('Starting Redux Examples');
+let stateDefault = { 
+    searchText: '', 
+    showCompleted: false, 
+    todos: []
+};
 
-let reducer = (state = { name: 'Anonymous' }, action) => {
+let reducer = (state=stateDefault, action) => {
     return state;
 }
 let store = createStore(reducer);
 
 let currentState = store.getState();
-console.log('currentState ', currentState);
+console.log('current state of store ', currentState);
